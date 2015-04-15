7d1897a031ebf30f3cc30ddc3089b58061de5530
Ivan Wang <icw@cmu.edu>
2015-04-15 00:29:41 -0400

    move hit thresholds to constants

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,12 +5,21 @@ public class Score {
     public string text;
     public int value = 0;
 
-    public const int PERFECT_SCORE = 5;
-    public const int WONDERFUL_SCORE = 4;
-    public const int GOOD_SCORE = 3;
-    public const int ALMOST_SCORE = 2;
-    public const int POOR_SCORE = 1;
-    public const int MISS_SCORE = 0;
+    // Delay thresholds for hitting notes
+    // TODO: finetune
+    const float ALMOST_THRESHOLD = -0.05f;
+    const float PERFECT_THRESHOLD = 0.25f;
+    const float WONDERFUL_THRESHOLD = 0.3f;
+    const float GOOD_THRESHOLD = 0.4f;
+    const float POOR_THRESHOLD = 0.5f;
+
+    // Score for hitting notes
+    const int PERFECT_SCORE = 5;
+    const int WONDERFUL_SCORE = 4;
+    const int GOOD_SCORE = 3;
+    const int ALMOST_SCORE = 2;
+    const int POOR_SCORE = 1;
+    const int MISS_SCORE = 0;
 
     private static Score perfect = new Score("PERFECT", PERFECT_SCORE);
     private static Score wonderful = new Score("WONDERFUL", WONDERFUL_SCORE);
@@ -47,16 +56,15 @@ public class Score {
 
     // Update score based on timing of hit
     public static Score ComputeScore(float delay) {
-        // TODO: finetune
-        if (delay < -0.05f) {
+        if (delay < ALMOST_THRESHOLD) {
             return Almost;
-        } else if (delay < 0.25f) {
+        } else if (delay < PERFECT_THRESHOLD) {
             return Perfect;
-        } else if (delay < 0.3f) {
+        } else if (delay < WONDERFUL_THRESHOLD) {
             return Wonderful;
-        } else if (delay < 0.4f) {
+        } else if (delay < GOOD_THRESHOLD) {
             return Good;
-        } else if (delay < 0.5f) {
+        } else if (delay < POOR_THRESHOLD) {
             return Poor;
         }
         return Miss;
