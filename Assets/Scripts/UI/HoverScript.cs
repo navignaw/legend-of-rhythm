61f7fde1282777f451319ddb2b2c003e270ce418
Roger Liu <rogliu26@gmail.com>
2015-05-06 01:07:05 -0400

    got the cow to go to the button on the level select screen

---
+++ Assets/Scripts/UI/HoverScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverScript : MonoBehaviour {
+
+    private Vector3 target;
+	// Use this for initialization
+	void Start () {
+        target = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if (transform.position != target)
+        {
+            float x = (target - transform.position).x;
+            float y = (target - transform.position).y;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(x * 5.0f, y * 5.0f);
+        }
+    }
+
+    public void SetTargetX(float x_position){
+        target.x = x_position;
+    }
+
+    public void SetTargetY(float y_position)
+    {
+        target.y = y_position;
+    }
+}
