3ae83ccee12ad1d3c9fb2070d14801e8db896f17
Ivan Wang <icw@cmu.edu>
2015-05-06 02:08:55 -0400

    update hover script and change cow to Image

--- Assets/Scripts/UI/HoverScript.cs
+++ Assets/Scripts/UI/HoverScript.cs
@@ -4,32 +4,27 @@ using System.Collections;
 public class HoverScript : MonoBehaviour {
 
     private Vector3 target;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         target = new Vector3(0, 0, 0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
 
     void FixedUpdate()
     {
         if (transform.position != target)
         {
-            float x = (target - transform.position).x;
-            float y = (target - transform.position).y;
+            float x = target.x - transform.position.x;
+            float y = target.y - transform.position.y;
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(x * 5.0f, y * 5.0f);
         }
     }
 
-    public void SetTargetX(float x_position){
-        target.x = x_position;
-    }
-
-    public void SetTargetY(float y_position)
-    {
-        target.y = y_position;
+    public void SetTarget(Transform transformTarget) {
+        target = transformTarget.position;
     }
 }
